5a5b64f7cf2e871f33e7b1d9d17f87fe449ab30b
guillaume plessy <bibipero.app@gmail.comgit>
2026-05-29 08:57:13 +0200

    Send state on socket connection

--- server.js
+++ server.js
@@ -54,6 +54,8 @@ let currentPlayerIndex = 0;
 io.on("connection", (socket) => {
 
   console.log("Joueur connecté :", socket.id);
+  socket.emit("stateUpdated", state);
+socket.emit("playersUpdated", connectedPlayers);
 
   socket.on("joinPlayer", (playerName) => {
 
